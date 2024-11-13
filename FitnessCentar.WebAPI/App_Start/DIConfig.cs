1cfcc5d77fa948d889fc2a4b62b1346d5b80ac89
Lsasko <luka.sasko123@gmail.com>
2024-11-13 15:06:59 +0100

    Initial project version

---
+++ FitnessCentar.WebAPI/App_Start/DIConfig.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using FitnessCentar.Repository;
+using FitnessCentar.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace FitnessCentar.WebAPI.App_Start
+{
+    public class DIConfig
+    {
+
+        public static void Register(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterModule( new ServiceModule());
+            builder.RegisterModule( new RepositoryModule());
+
+            builder.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            var contrainer = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(contrainer);
+
+        }
+    }
+}
---
+++ FitnessCentar.WebAPI/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FitnessCentar.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
