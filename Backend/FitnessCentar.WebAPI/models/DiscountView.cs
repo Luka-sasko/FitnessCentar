39df8391fd1036b6ca04ae5c2df9d498b617128e
Lsasko <luka.sasko123@gmail.com>
2024-11-20 14:08:17 +0100

    GetAllDiscount + filter paging sorting

--- Backend/FitnessCentar.WebAPI/models/DiscountView.cs
+++ Backend/FitnessCentar.WebAPI/models/DiscountView.cs
@@ -7,8 +7,10 @@ namespace FitnessCentar.WebAPI.Models
 {
     public class DiscountView
     {
+        public Guid Id {  get; set; }
         public string Name { get; set; }
         public int Amount { get; set; }
+        public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
 }
