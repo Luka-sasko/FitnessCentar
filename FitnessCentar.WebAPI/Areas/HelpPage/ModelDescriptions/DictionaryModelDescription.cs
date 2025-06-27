2c46a2ca909c3009f0a81daba1a654033344fd29
Lsasko <luka.sasko123@gmail.com>
2025-06-27 22:29:08 +0200

    Revert "Initial project version"
    This reverts commit f0e45a0323d667c01d3dc1512aad9ffc41cb2b7b.

--- FitnessCentar.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FitnessCentar.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- FitnessCentar.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FitnessCentar.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
